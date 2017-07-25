f2ab614205c9af653f42799c44e2f2f3d53c58c0
Tyler Speaks <speaks_tyler@yahoo.com>
2017-07-24 20:40:23 -0500

    Initial upload of HTML/CSS and JS files

---
+++ app.js
@@ -0,0 +1,45 @@
+$(document).ready(function () { 
+	function getResults(searchTerm) {
+		$.ajax(
+			{
+				url: `https://omgvamp-hearthstone-v1.p.mashape.com/cards/search/${searchTerm}?collectible=1`,
+				beforeSend: setHeaders
+						
+			}
+		)
+			.then(function(data) {
+				let cards = data.map((i) => {
+					return cardTemplate(i);
+				})
+				displayResults(cards);
+			})
+		
+			.fail(function(err) {
+				console.log(err);
+			})
+
+	}
+
+	function setHeaders(xhr) {
+		xhr.setRequestHeader('X-Mashape-Key', 'frPNjBhS3hmshGNhH2wxsLdN72HKp14KgwTjsnftjcJxESuISw');
+	}
+
+
+	 function cardTemplate(data) {
+	 	return `<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+	 					<img src="${data.img}" />	
+	 			</div>`
+
+	 }
+
+	function displayResults(cards) {
+		$("#search-results").html(cards);
+	}
+
+	//search term
+	$("#search-form").submit(function (event) {
+		event.preventDefault();
+		getResults($("#search-term").val());
+		console.log("This worked");
+	});
+});
